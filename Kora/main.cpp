b49a2dae11aa41a3d6cf9f26458280bea88c6135
Ian Medland <imedland@me.com>
2014-02-22 11:10:04 +0000

    Initial Commit

---
+++ Kora/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Kora
+//
+//  Created by Ian Medland on 22/02/2014.
+//  Copyright (c) 2014 Ian Medland. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
